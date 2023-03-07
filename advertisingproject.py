d4a7ca755e6980abcbbbc61ea26de9fa6651273d
hairaniabmalik <126454904+hairaniabmalik@users.noreply.github.com>
2023-03-07 15:33:33 +0800

    Update advertisingproject.py

--- advertisingproject.py
+++ advertisingproject.py
@@ -9,6 +9,13 @@ st.write("""
 This app predicts the **Advertisement** type!
 """)
 
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
