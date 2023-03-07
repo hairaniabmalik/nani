1ca19c0d069ae4c10bf6f7b835eec974756dc08f
hairaniabmalik <126454904+hairaniabmalik@users.noreply.github.com>
2023-03-07 12:04:56 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -15,14 +15,14 @@ if option=='line chart':
 
     st.line_chart(chart_data)
 
-    elif option=='map':
+ elif option=='map':
     map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
     st.map(map_data)
     
-    else:
+ else:
     st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
     show = st.checkbox('I agree the terms and conditions')
     if show:
