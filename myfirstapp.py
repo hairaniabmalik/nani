6dfb09858a7070794fd33c98e2dfc0ff18493cc5
hairaniabmalik <126454904+hairaniabmalik@users.noreply.github.com>
2023-03-07 12:03:46 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -4,11 +4,31 @@ import pandas as pd
 
 st.header("My first Streamlit App")
 
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C'])
 
-if show:
-    st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+if option=='line chart':
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+    elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.map(map_data)
+    
+    else:
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+        'Intplan': ['yes', 'yes', 'yes', 'no'],
+        'Churn Status': [0, 0, 0, 1]
+        }))
+        
+        
