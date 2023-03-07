6651d58797e55f4848b5941c29a3a798342cdeee
hairaniabmalik <126454904+hairaniabmalik@users.noreply.github.com>
2023-03-07 15:03:31 +0800

    Add files via upload

---
+++ advertisingproject.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("My first Streamlit App")
+st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
